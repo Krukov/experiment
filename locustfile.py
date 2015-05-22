0c817e8385ed0822adf0512df7600f7380a83973
Krukov <Glebov.ru@gmail.com>
2015-05-22 19:20:16 +0300

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,32 @@
-from locust import HttpLocust, TaskSet
+#!/usr/bin/python2.7
+
+import random
+import string
+
+from locust import HttpLocust, TaskSet, task
 
 
-def index(l):
-    l.client.get("/")
+class Tasks(TaskSet):
 
-def profile(l):
-    l.client.get("/profile")
+    @task(5)
+    def all_tasks(self):
+        self.client.get('/tasks')
 
-class TasksBehavior(TaskSet):
-    tasks = {index: 5, profile: 5}
+    @task(5)
+    def task_by_id(self):
+        for i in range(10):
+            self.client.get('/tasks/%s' % i, name='/tasks/[id]')
+
+    @task(10)
+    def add_tasks(self):
+        self.client.post('/tasks/add',
+                         {
+                             'title': ''.join([random.choice(string.ascii_letters) for _ in xrange(32)]),
+                             'body': ''.join([random.choice(string.ascii_letters) for _ in xrange(100)])
+                         })
 
 
 class WebsiteTasks(HttpLocust):
-    task_set = TasksBehavior
-    min_wait=5000
-    max_wait=9000
+    task_set = Tasks
+    min_wait = 5000
+    max_wait = 9000
